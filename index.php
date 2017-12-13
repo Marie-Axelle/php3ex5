23cb667352035477eff8048d6adf28649a0c84b3
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 11:12:49 +0100

    boucle 5

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $onebyone= 0;
+
+    while ($onebyone <= 15) {
+      echo ' On y arrive presque '. $onebyone++;
+    }
+     ?>
+  </body>
+</html>
